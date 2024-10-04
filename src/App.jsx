9535fc408e319e733f1bc8e50970ad113a26555f
kira0826 <curaca492@gmail.com>
2024-10-04 14:19:08 -0500

    build: files system creation.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Typography } from '@material-tailwind/react'
+
+function App() {
+
+  return (
+    <>
+      
+      <Typography> Prueba de Tailwind</Typography>
+
+      <div className= "flex flex-row justify-center">
+      <Typography> Prueba de tailwind 2, uso de clases</Typography>
+
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
